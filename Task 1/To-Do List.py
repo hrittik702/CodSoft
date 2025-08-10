f1de236d3e71a91a8ab6218f2f263e7b7ec8e014
Hrittik <mmhrittik@gmail.com>
2025-08-10 20:45:51 +0530

    Adding Readme and Menu

--- Task 1/To-Do List.py
+++ Task 1/To-Do List.py
@@ -3,7 +3,9 @@ tasks = []
 
 #main menu
 print("------ To-Do Lists Main Menu --------")
-print("View Tasks")
-print("Add Task")
-print("Mark Task as Completed")
-print()
+print("1. View Tasks")
+print("2. Add Task")
+print("3. Mark Task as Completed")
+print("4. Delete Task")
+print("5. Exit")
+
