0c7957f4c276b96714219dd70133ce41247e7ef2
Hrittik <mmhrittik@gmail.com>
2025-08-10 21:00:53 +0530

    Adding view Task feature

--- Task 1/To-Do List.py
+++ Task 1/To-Do List.py
@@ -2,10 +2,20 @@
 tasks = []
 
 #main menu
-print("------ To-Do Lists Main Menu --------")
-print("1. View Tasks")
-print("2. Add Task")
-print("3. Mark Task as Completed")
-print("4. Delete Task")
-print("5. Exit")
+def menu():
+    print("------ To-Do Lists Main Menu --------")
+    print("1. View Tasks")
+    print("2. Add Task")
+    print("3. Mark Task as Completed")
+    print("4. Delete Task")
+    print("5. Exit")
+
+#defining each menu
+def view_task():
+    if not tasks:
+        print("There is no Tasks")
+    else :
+        for i, task in enumerate(tasks, start = 1):
+            print(f"{i}.{task}")
+
 
