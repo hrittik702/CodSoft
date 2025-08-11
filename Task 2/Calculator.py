d91e7cccf8b89bbbb9af188385d0efa2de51084d
Hrittik <mmhrittik@gmail.com>
2025-08-11 11:50:38 +0530

    basic working of calculator

--- Task 2/Calculator.py
+++ Task 2/Calculator.py
@@ -1,8 +1,3 @@
-a,b = 0,0
-
-def num():
-    a = int(input("Enter First no. : "))
-    b = int(input("Enter Second no. : "))
 
 def menu():
     print("\n")
@@ -14,14 +9,38 @@ def menu():
     print("5. Remainder")
     print("6. Sqaure")
     print("7. Cube")
-
-def add():
-    print(a+b)
-
+    print("8. Exit")
 
 while True :
     menu()
     choice = int(input("Choose a option : "))
     if choice == 1:
-        num()
-        print(a+b)
+        a = float(input("Enter First no. : "))
+        b = float(input("Enter Second no. : "))
+        print("Sum = ",a+b)
+    elif choice == 2:
+        a = float(input("Enter First no. : "))
+        b = float(input("Enter Second no. : "))
+        print("Subtraction = ",a-b)
+    elif choice == 3:
+        a = float(input("Enter First no. : "))
+        b = float(input("Enter Second no. : "))
+        print("Product = ",a*b)
+    elif choice == 4:
+        a = float(input("Enter First no. : "))
+        b = float(input("Enter Second no. : "))
+        print("Division = ",a/b)
+    elif choice == 5:
+        a = float(input("Enter First no. : "))
+        b = float(input("Enter Second no. : "))
+        print("Remainder = ",a%b)
+    elif choice == 6:
+        a = float(input("Enter no. : "))
+        print("Sqaure = ",a**2)
+    elif choice == 7:
+        a = float(input("Enter no. : "))
+        print("Cube = ",a**3)
+    elif choice == 8:
+        break
+    else :
+        print("Invalid option, Please choose a valid option")
